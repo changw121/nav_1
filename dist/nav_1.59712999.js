2e9ce151cd4d2e4d63852f0ee7fc33215b36eed3
changw <2283992824@qq.com>
2022-10-09 21:26:58 +0800

    add yarn build

--- dist/nav_1.59712999.js
+++ dist/nav_1.59712999.js
@@ -533,6 +533,6 @@ function hmrAcceptRun(bundle, id) {
 
 },{}],"4M6V8":[function(require,module,exports) {
 
-},{}]},["142Ks","4M6V8"], "4M6V8", "parcelRequire94c2")
+},{}]},["142Ks","4M6V8"], "4M6V8", "parcelRequire2914")
 
 //# sourceMappingURL=nav_1.59712999.js.map
---
+++ src/readme.md
@@ -0,0 +1,12 @@
+# 开发
+
+```
+yarn global add parcel-bundler
+parcel src/index.html
+```
+注：有可能需要在parcel前加一个npx
+
+# build 命令
+```
+yarn build
+```
